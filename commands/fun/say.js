540429deede51a30e32af6fd1ed99928d12d4139
mnmrepl <87793659+mnmrepl@users.noreply.github.com>
2021-09-19 16:57:57 -1000

    Delete commands/fun directory

--- commands/fun/say.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'say',
-  description: 'Repeats what you say',
-  run: async (client, message, args, Discord) => {
-    const sayM = args.join(' ');
-    message.delete().catch(err => console.log(err))
-    message.channel.send({ content: sayM})
-  }
-}
