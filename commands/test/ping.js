60ec1286edcdbd22753ca0c243451954d233bcf3
mnmrepl <87793659+mnmrepl@users.noreply.github.com>
2021-09-19 17:00:29 -1000

    Update ping.js

--- commands/test/ping.js
+++ commands/test/ping.js
@@ -3,6 +3,7 @@ const { MessageEmbed } = require('discord.js')
 module.exports = {
   name: 'ping',
   description: 'Displays the bots ping',
+  cooldown: 1000 * 20 * 1,
   run: async (client, message, args, Discord) => {
     const embed = new MessageEmbed()
     .setTitle('Pong!')
