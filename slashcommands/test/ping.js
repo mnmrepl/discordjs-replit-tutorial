889d1efe88e3d8688557e87b52d9bb23f916d7af
mnmrepl <87793659+mnmrepl@users.noreply.github.com>
2021-09-19 17:40:48 -1000

    Delete slashcommands directory

--- slashcommands/test/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'ping',
-  description: 'Displays the bots ping',
-  run: async (client, interaction, options) => {
-    await interaction.followUp({ content: 'pong!'})
-  }
-}
