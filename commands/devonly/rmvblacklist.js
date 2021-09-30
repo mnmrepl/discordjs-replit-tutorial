d0f586454c4b45408fab474aa59d2c339fee902b
mnmrepl <87793659+mnmrepl@users.noreply.github.com>
2021-09-29 17:30:41 -1000

    Update rmvblacklist.js

--- commands/devonly/rmvblacklist.js
+++ commands/devonly/rmvblacklist.js
@@ -3,7 +3,7 @@ const blacklist = require('../../models/userBlacklist')
 module.exports = {
   name: 'rmvblacklist',
   run: async (client, message, args, Discord) => {
-    if (message.author.id !== '794359225157550140') return message.reply({ content: 'Only my developer can use this command!'})
+    if (message.author.id !== 'your id here') return message.reply({ content: 'Only my developer can use this command!'})
     const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     if (!user) return message.reply({ content: 'Please specify someone to remove the blacklist'})
 
