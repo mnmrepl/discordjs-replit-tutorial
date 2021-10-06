1245e6b235fead25a12fe7ae0149685db66cf225
mnmrepl <87793659+mnmrepl@users.noreply.github.com>
2021-10-05 18:30:52 -1000

    Create accept.js

---
+++ commands/moderation/accept.js
@@ -0,0 +1,56 @@
+const { MessageEmbed } = require('discord.js')
+const db = require('quick.db')
+
+module.exports = {
+  name: 'accept',
+  description: 'Accepts a suggestion',
+  UserPerms: ['ADMINISTRATOR'],
+  run: async (client, message, args, Discord) => {
+    let channel = await db.fetch(`suggestion_${message.guild.id}`)
+    if (channel === null) return message.reply({ content: 'There is no set channel for suggestions'})
+
+    if (channel === null) return
+    const rgx = /^(?:<@!?)?(\d+)>?$/
+    const messageID = args[0]
+    const replyQ = args.slice(1).join(' ') || 'No reason given.'
+
+    if (!messageID) return message.reply({ content: 'Please specify the ID of the message.'})
+
+    if (!rgx.test(messageID)) return message.reply({ content: 'Please specify the ID of the message.'}) 
+
+    try {
+      const suggestionC = message.guild.channels.cache.get(channel)
+      if (!suggestionC) return message.channel.send({ content: 'No suggestion channel was found.'})
+
+      const suggested = await suggestionC.messages.fetch(messageID).catch(error => {
+        message.channel.send({ content: 'No message was found.'})
+      })
+
+      const data = suggested.embeds[0]
+
+      const replyE = new MessageEmbed()
+      .setAuthor(`${data.author.name}`, `${data.author.iconURL}`)
+      .setTitle(`Suggestion!`)
+      .setDescription(`${data.description}`)
+      .setColor("GREEN")
+      .addField(`Accepted by ${message.author.tag}`, replyQ)
+      .setFooter(`Status: accepted`)
+
+      suggested.edit({ embeds: [replyE]})
+      suggested.reactions.removeAll()
+      message.channel.send({ content: 'Successfully accepted the suggestion.'})
+      // we can send a message to let the user know their suggestion was accepted or declined.
+
+      const user = await client.users.cache.find((u) => u.tag === data.author.name)
+
+      const embed1 = new MessageEmbed()
+      .setDescription(`Your [suggestion](https://discord.com/channels/${message.guild.id}/${channel}/${messageID}) has been accepted.`)
+      .setColor("GREEN")
+    // the description will be a link to their suggestion
+      user.send({ embeds: [embed1]})
+    } catch (err) {
+      console.log(err)
+      return
+    }
+  }
+}
