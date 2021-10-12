edfb65fcc226c8978c9bb793bd44ac44883dda2e
mnmrepl <87793659+mnmrepl@users.noreply.github.com>
2021-10-12 09:49:14 -1000

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
 const { Collection, Client } = require('discord.js')
 const client = new Client({ intents: 32767 })
 const server = require('./server.js')
-const mongoose = require('mongoose');
-mongoose.connect(process.env['mongo'], { useNewUrlParser: true, useUnifiedTopology: true }).then(console.log('Connected to the database.'));
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://zox:Cminami.12@database.xcmtd.mongodb.net/zox?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true}).then(console.log('connected to the database'))
+const schema = require('./models/economy')
 
 const prefix = process.env['prefix']
 
 const token = process.env['token']
 
-module.exports = client;
 
-require(`./handler`)(client)
-require('./handler/slashcommands')
+module.exports = client;
 
 client.commands = new Collection()
 client.aliases = new Collection()
 client.slashcommands = new Collection()
 
-client.on('ready', () => {
-  console.log('i am online')
+require('./handler')(client)
+require('./handler/slashcommands')
+
+
+client.bal = (User) => new Promise(async ful => {
+  const data = await schema.findOne({ User   })
+    if (!data) return ful(0)
+    if (!data.Wallet || data.Wallet === null) return ful(0)
+    ful(data.Wallet)
 })
+  client.add = (User, Money) => {
+    schema.findOne({
+      User
+    }, async (err,data) => {
+      if (err) throw err
+      if (data) {
+        data.Wallet += Money
+      } else {
+        data = new schema ({ User: User, Wallet: Money})
+      }
+      data.save()
+    })
+  }
+  client.rmv = (User, Money) => {
+    schema.findOne({ User }, async (err, data) => {
+      if (err) throw err
+      if (data) {
+        data.Wallet -= Money
+      } else {
+        data = new schema({ User: User, Wallet: -Money })
+      }
+      data.save()
+    })
+  }
+  client.bankBal = (User) => new Promise(async ful => {
+    const data = await schema.findOne({ User })
+    if (!data) return ful(0)
+    if (!data.Bank) return ful(0)
+    ful(data.Bank)
+  })
+  client.bankDeposit = (User, Money) => {
+    schema.findOne({ User}, async (err, data) => {
+      if (err) throw err
+      if (data) {
+        data.Bank += Money
+      } else {
+        data = new schema({ User: User, Bank: Money })
+      } data.save()
+    })
+  }
+  client.bankWithdraw = (User, Money) => {
+    schema.findOne({ User }, async (err, data) => {
+      if (err) throw err
+      if (data) {
+        data.Bank -= Money
+      } else {
+        data = new schema({ User: User, Bank: -Money})
+      }
+      data.save()
+    })
+  }
 
 client.login(token)
